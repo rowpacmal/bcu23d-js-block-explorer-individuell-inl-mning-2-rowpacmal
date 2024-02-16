db2a51e819db8ba8a510f13666bfdffa9d1397d8
Rowel Malmström <143719030+rowpacmal@users.noreply.github.com>
2024-02-16 20:59:10 +0100

    experiment with the app

--- scripts/lib/app.js
+++ scripts/lib/app.js
@@ -7,9 +7,24 @@ const transactionAmountInput = document.querySelector('#transaction-amount');
 const receiverAddressInput = document.querySelector('#receiver-address');
 const sendTransactionButton = document.querySelector('#send-transaction');
 
+let test = null;
+test = true;
+
+if (test) {
+  console.log('ok');
+} else {
+  console.log('no');
+}
+
 async function initApp() {
   if (typeof ethereum !== 'undefined') {
     console.info('MetaMask is installed!');
+
+    const wallet = await ethereum.request({ method: 'eth_requestAccounts' });
+
+    if (wallet) {
+      console.info('Wallet is connected!');
+    }
   } else {
     console.warn('MetaMask is not installed!');
   }
@@ -28,25 +43,29 @@ async function checkWallet() {
   if (typeof ethereum !== 'undefined') {
     await ethereum.request({ method: 'eth_requestAccounts' });
 
-    const balance = await ethereum.request({
-      method: 'eth_getBalance',
-      params: [walletAddressInput.value, 'latest'],
-    });
+    if (walletAddressInput.value) {
+      const balance = await ethereum.request({
+        method: 'eth_getBalance',
+        params: [walletAddressInput.value, 'latest'],
+      });
 
-    const parseBalance = parseInt(balance) / Math.pow(10, 18);
+      const parseBalance = parseInt(balance) / Math.pow(10, 18);
 
-    balanceDisplay.innerHTML = `<i class="fa-brands fa-ethereum"></i> ${parseBalance} ETH`;
+      balanceDisplay.innerHTML = `<i class="fa-brands fa-ethereum"></i> ${parseBalance} ETH`;
 
-    const block = await ethereum.request({
-      method: 'eth_getBlockByNumber',
-      params: ['latest', true],
-    });
+      const block = await ethereum.request({
+        method: 'eth_getBlockByNumber',
+        params: ['latest', true],
+      });
 
-    const blockNumber = parseInt(block.number);
-    const transactions = block.transactions;
+      const blockNumber = parseInt(block.number);
+      const transactions = block.transactions;
 
-    if (block !== null && transactions !== null) {
-      displayHistory(transactions);
+      if (block !== null && transactions !== null) {
+        displayHistory(transactions);
+      }
+    } else {
+      console.warn('Warning: "Explorer" has an empty field!');
     }
   }
 }
@@ -78,24 +97,34 @@ async function sendFunds() {
   if (typeof ethereum !== 'undefined') {
     await ethereum.request({ method: 'eth_requestAccounts' });
 
-    try {
-      const fund = parseFloat(transactionAmountInput.value) * Math.pow(10, 18);
-      const response = await ethereum.request({
-        method: 'eth_sendTransaction',
-        params: [
-          {
-            to: receiverAddressInput.value,
-            from: senderAddressInput.value,
-            value: Number(fund).toString(16),
-            gas: Number(21000).toString(16),
-            gasPrice: Number(2502020).toString(16),
-          },
-        ],
-      });
+    if (
+      senderAddressInput.value &&
+      transactionAmountInput.value &&
+      receiverAddressInput.value
+    ) {
+      try {
+        const fund =
+          parseFloat(transactionAmountInput.value) * Math.pow(10, 18);
 
-      console.info(response);
-    } catch (error) {
-      console.error(error);
+        const response = await ethereum.request({
+          method: 'eth_sendTransaction',
+          params: [
+            {
+              to: receiverAddressInput.value,
+              from: senderAddressInput.value,
+              value: Number(fund).toString(16),
+              gas: Number(21000).toString(16),
+              gasPrice: Number(2502020).toString(16),
+            },
+          ],
+        });
+
+        console.info(response);
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      console.warn('Warning: "Exchanger" has one or more empty fields!');
     }
   }
 }
