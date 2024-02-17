129c2eb733bdb170f05dd3df1f92b2af73b2b601
Rowel Malmström <143719030+rowpacmal@users.noreply.github.com>
2024-02-17 19:29:19 +0100

    selected chain warning message added

--- scripts/lib/app.js
+++ scripts/lib/app.js
@@ -6,6 +6,7 @@ import { appNoWallet } from './nowallet.js';
 import { appWelcome } from './welcome.js';
 
 const rootElement = document.querySelector('#root');
+const warningElement = document.querySelector('#warning');
 
 async function initApp() {
   if (typeof ethereum !== 'undefined') {
@@ -18,10 +19,30 @@ async function initApp() {
       buildWelcome(rootElement);
       appWelcome();
     }
+
+    const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+
+    if (chainId !== '0xaa36a7') {
+      notOnSepoliaTestnet();
+    }
   } else {
     buildNoWallet(rootElement);
     appNoWallet();
   }
+}
+
+function notOnSepoliaTestnet() {
+  warningElement.innerHTML =
+    '<p>You are currently connected to a different blockchain than "Sepolia Testnet." To ensure the safety of your funds, it is strongly recommended to switch to the "Sepolia Testnet" before using this application.</p>';
+  warningElement.style.opacity = 1;
+
+  setTimeout(() => {
+    warningElement.style.opacity = 0;
+
+    setTimeout(() => {
+      warningElement.innerHTML = '';
+    }, 3000);
+  }, 30000);
 }
 
 function reloadApp() {
@@ -30,3 +51,4 @@ function reloadApp() {
 
 document.addEventListener('DOMContentLoaded', initApp);
 ethereum.on('accountsChanged', reloadApp);
+ethereum.on('chainChanged', reloadApp);
